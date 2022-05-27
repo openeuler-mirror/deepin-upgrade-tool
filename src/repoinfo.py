1c6e54dfdcd0f0ada27208934fbe08cf1d515908
单身情歌依旧唱 <wangshu@uniontech.com>
2022-05-27 13:15:38 +0800

    格式化代码
    Signed-off-by: 单身情歌依旧唱 <wangshu@uniontech.com>

--- src/repoinfo.py
+++ src/repoinfo.py
@@ -8,38 +8,44 @@ import signal
 
 LOGFILE = "/var/log/repoinfomation.log"
 RECOARDDIR = "/var/infomation"
-RECOARDFILE = "msg.txt"
+RECOARDFILE = os.path.join(RECOARDDIR, "msg.txt")
+logging.basicConfig(filename=LOGFILE, level=logging.DEBUG)
 
 
 def handler(sig, frame):
     delfile(RECOARDFILE)
     exit()
 
-logging.basicConfig(filename = LOGFILE,level = logging.DEBUG)
+
 signal.signal(signal.SIGTERM, handler)
+
 
 def myrecoard(content):
     if not os.path.exists(RECOARDDIR):
-        os.mkdir("/var/infomation/")
+        os.mkdir(RECOARDDIR)
 
     with open(RECOARDFILE, "w") as f:
         f.write(content)
+
+
 def delfile(filename):
     if os.path.exists(filename):
         try:
             os.remove(filename)
         except OSError:
             pass
+
+
 def createlogfile():
     num = 0
     FNULL = open(os.devnull, 'w')
-    proc=subprocess.Popen('/usr/bin/dnf-3 check-update',
-            shell=True,
-            stdin=None,
-            stdout=subprocess.PIPE,
-            stderr=FNULL)
+    proc = subprocess.Popen('/usr/bin/dnf-3 check-update',
+                            shell=True,
+                            stdin=None,
+                            stdout=subprocess.PIPE,
+                            stderr=FNULL)
     try:
-        outs, errs = proc.communicate(timeout = 60 )
+        outs, errs = proc.communicate(timeout=60)
     except subprocess.TimeoutExpired:
         proc.kill()
         logging.info('Exec command timeout')
@@ -54,14 +60,17 @@ def createlogfile():
         if re.search("uel20", line):
             num += 1
     logging.info('Connect repo server success')
-    msg="Upgradable packages:"+str(num)
+    msg = "Upgradable packages:" + str(num)
     myrecoard(msg)
     logging.info(msg)
+
 
 def main():
     logging.info("Main fun start")
     createlogfile()
     logging.info("Main fun end")
 
+
 if __name__ == '__main__':
-    main()
+    print(RECOARDFILE)
+    # main()
