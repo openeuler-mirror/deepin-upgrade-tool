0690416bee4ac74839fb0f7f8acf20def8e69513
单身情歌依旧唱 <wangshu@uniontech.com>
2022-05-27 14:02:47 +0800

    Fix bug
    Signed-off-by: 单身情歌依旧唱 <wangshu@uniontech.com>

---
+++ utrepoinfo/qss.py
@@ -0,0 +1,84 @@
+from utrepoinfo.config import DATAPATH
+
+qss_style = '''
+QPushButton#close
+{
+    border-style:none;
+    background-image: url(%s/img/window_close_normal_light.png);
+    background-position:center;
+    background-repeat:no-repeat;
+}
+
+QPushButton#close::hover
+{
+	background-color: #E6E6E6;
+
+}
+
+QPushButton#hide
+{
+    border-style:none;
+    background-image: url(%s/img/window_min_normal_light.png);
+    background-position:center;
+    background-repeat:no-repeat;
+}
+
+QPushButton#hide::hover
+{
+	background-color: #E6E6E6;
+
+}
+
+QPushButton[name~='btn']
+{
+    width: 170px;
+    height: 36px;
+    background: rgba(0,0,0,0.08);
+    border: 1px solid rgba(0,0,0,0.03);
+    border-radius: 8px;
+    box-shadow: 0px 4px 4px 0px rgba(0,145,255,0.30); 
+}
+
+QPushButton#cancle
+{
+	background-color: #E6E6E6;
+}
+
+QPushButton#cancle::hover
+{
+	background-color: #C0C0C0;
+}
+
+QPushButton#update
+{
+	background-color: #0091FF;
+	color: #FFFFFF;
+}
+
+QPushButton#update::hover
+{
+	background-color: #0081FF;
+}
+
+QTextBrowser
+{
+    background: rgba(255,255,255,0.50);
+    border: 1px solid rgba(0,0,0,0.10);
+    border-radius: 8px;
+    font-size: 12px;
+    font-family: Noto Mono;
+}
+
+QTableWidget
+{
+    background: #ffffff;
+    border: 1px solid rgba(0,0,0,0.10);
+    border-radius: 8px;
+    alternate-background-color: rgb(244,244,244);text-align:right;
+}
+
+QHeaderView::section
+{
+    border-width:6;
+}
+''' % (DATAPATH, DATAPATH)
--- utrepoinfo/window.py
+++ utrepoinfo/window.py
@@ -7,113 +7,7 @@ from PyQt5.QtGui import QFont, QPixmap, QCursor, QIcon
 from PyQt5.QtCore import Qt, QRect, QMetaObject, QCoreApplication, QEvent, QObject, pyqtSignal, QProcess, QTimer
 from utrepoinfo.config import RPMPKGSDETAILS, LOGOPNG
 from utrepoinfo.utils import read_jsonfile_to_pyobj
-
-qssStyle = '''
-QPushButton#close
-{
-    border-style:none;
-    background-image: url(img/window_close_normal_light.png);
-    background-position:center;
-    background-repeat:no-repeat;
-}
-
-QPushButton#close::hover
-{
-	background-color: #E6E6E6;
-
-}
-
-QPushButton#hide
-{
-    border-style:none;
-    background-image: url(img/window_min_normal_light.png);
-    background-position:center;
-    background-repeat:no-repeat;
-}
-
-QPushButton#hide::hover
-{
-	background-color: #E6E6E6;
-
-}
-
-QPushButton[name~='btn']
-{
-    width: 170px;
-    height: 36px;
-    background: rgba(0,0,0,0.08);
-    border: 1px solid rgba(0,0,0,0.03);
-    border-radius: 8px;
-    box-shadow: 0px 4px 4px 0px rgba(0,145,255,0.30); 
-}
-
-QPushButton#cancle
-{
-	background-color: #E6E6E6;
-}
-
-QPushButton#cancle::hover
-{
-	background-color: #C0C0C0;
-}
-
-QPushButton#update
-{
-	background-color: #0091FF;
-	color: #FFFFFF;
-}
-
-QPushButton#update::hover
-{
-	background-color: #0081FF;
-}
-
-QTextBrowser
-{
-    background: rgba(255,255,255,0.50);
-    border: 1px solid rgba(0,0,0,0.10);
-    border-radius: 8px;
-    font-size: 12px;
-    font-family: Noto Mono;
-}
-
-QTableWidget
-{
-    background: #ffffff;
-    border: 1px solid rgba(0,0,0,0.10);
-    border-radius: 8px;
-    alternate-background-color: rgb(244,244,244);text-align:right;
-}
-
-QHeaderView::section
-{
-    border-width:6;
-}
-'''
-"""
-QHeaderView::section
-{
-    border: 1px solid red;
-    margin: 1px;
-    text-align: right;
-    font-family: arial;
-}
-QPushButton[name~='btn']::hover
-{
-	background-color: #0091FF;
-}
-QCheckBox::indicator:unchecked{  
-    image:url(img/checkbox_unchecked_insensitive.png);  
-}  
-  
-QCheckBox::indicator:checked{  
-    image:url(img/checkbox_checked_insensitive.png);  
-}  
-  
-QCheckBox::indicator:checked:disabled{  
-    image:url(img/checkbox_unchecked_insensitive.png);  
-}  
-"""
+from utrepoinfo.qss import qss_style
 
 
 class Ui_rpm_update(QMainWindow):
@@ -209,7 +103,7 @@ class Ui_rpm_update(QMainWindow):
         self.process.finished.connect(self.stop_install)
         # Init QSystemTrayIcon
         self.repo_tray()
-        self.setStyleSheet(qssStyle)
+        self.setStyleSheet(qss_style)
         self.retranslateUi(self)
         QMetaObject.connectSlotsByName(self)
 
