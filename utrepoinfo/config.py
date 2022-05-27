38ddce246d0793bb83cb82fbd4dfc5e727f598b4
单身情歌依旧唱 <wangshu@uniontech.com>
2022-05-27 13:23:20 +0800

    update repoinfo
    Signed-off-by: 单身情歌依旧唱 <wangshu@uniontech.com>

--- utrepoinfo/config.py
+++ utrepoinfo/config.py
@@ -2,3 +2,4 @@ import os
 LOGFILE = "/var/log/repoinfomation.log"
 RECOARDDIR = "/var/infomation"
 RECOARDFILE = os.path.join(RECOARDDIR, "msg.txt")
+RPMPKGSDETAILS = os.path.join(RECOARDDIR, "pkgs.json")
